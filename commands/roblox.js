2a4a98c4ae25ee77148d4a237cb3cb2e2cc4bb1a
Trooper-Toaster <46659742+Trooper-Toaster@users.noreply.github.com>
2019-03-24 22:02:18 -0400

    Update roblox.js

--- commands/roblox.js
+++ commands/roblox.js
@@ -3,11 +3,13 @@ const roblox = require("roblox-js");
 
 exports.run = (bot, message, args) => {
   
-  let person = args[0]
+  let person = args[0];
+  let reason = args[1]
+   let proof = args[2]
   
   roblox.getIdFromUsername(person).then(foundId => {
             const Id = foundId
-            message.channel.send(`Hello \n \n ${Id}`)
+            message.channel.send(`Username | Id: ${person} | Id \n \nProfile Link: https://www.roblox.com/users/${Id}/profile \n \nReason for the ban: ${reason} \n \nProof: ${proof} `)
             });
   message.channel.send
   
